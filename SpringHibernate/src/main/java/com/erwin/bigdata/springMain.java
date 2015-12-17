af86d58a094ea59f37da4c695cd649083dda376e
yanbo06 <yanbo06@ca.com>
2015-12-17 11:07:16 +0800

    re

--- SpringHibernate/src/main/java/com/erwin/bigdata/springMain.java
+++ SpringHibernate/src/main/java/com/erwin/bigdata/springMain.java
@@ -2,6 +2,6 @@ package com.erwin.bigdata;
 
 public class springMain {
 	public static void main(String[] argv){
-		System.out.println("Hello GitHub!");
+		System.out.println("Hello GitHub! wewe");
 	}
 }
